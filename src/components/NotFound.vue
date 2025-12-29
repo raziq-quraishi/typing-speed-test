7e0cb2a671753613d814dd2d082e7d8550c0fafe
Abdul Raziq Quraishi <99569578+raziq-quraishi@users.noreply.github.com>
2025-12-29 18:09:30 +0430

    Added Not Found Page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="flex flex-col items-center justify-center h-screen text-amber-50">
+    <h1 class="text-6xl mb-4">404</h1>
+    <p class="text-2xl">Page Not Found</p>
+  </main>
+</template>
+<script setup></script>
+<style scoped></style>
